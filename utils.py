36acc1a669b7c3c9a9e3931ad67cd2d75bd6d8ed
Camilo Valencia <camilo.akimushkin@gmail.com>
2026-06-27 15:18:58 -0300

    v1.0 ugly layout complete for resume_miranda, no photo

--- utils.py
+++ utils.py
@@ -11,17 +11,20 @@ def load_yaml(filepath):
     """Load YAML file and convert jobs data to list format for Jinja2"""
     with open(filepath, 'r') as f:
         data = yaml.safe_load(f)
-    
-    jobs = []
-    for job_key in sorted(data['jobs'].keys(), key=lambda x: int(x)):
-        job = data['jobs'][job_key].copy()
-        if 'items' in job:
-            job['items'] = list(job['items'].values())
-        else:
-            job['items'] = []
-        jobs.append(job)
-    data['jobs'] = jobs
-    
+
+    selected_sections = {'jobs', 'education'}  #, 'publications', 'projects', etc
+    selected_sections = selected_sections.intersection(data.keys())
+    for section in selected_sections:
+        subsections = []
+        for subsection_key in sorted(data[section].keys(), key=lambda x: int(x)):
+            subsection = data[section][subsection_key].copy()
+            if 'items' in subsection:
+                subsection['items'] = list(subsection['items'].values())
+            else:
+                subsection['items'] = []
+            subsections.append(subsection)
+        data[section] = subsections
+
     return data
 
 def render_template(template_path, data):
@@ -34,7 +37,7 @@ def render_template(template_path, data):
 def main():
     data = load_yaml('resume_miranda.yaml')
     rendered = render_template('raw/resume_miranda.tex', data)
-    
+
     os.makedirs('filled', exist_ok=True)
     with open('filled/resume_miranda.tex', 'w') as f:
         f.write(rendered)
